d5a2c5823c1b7413a914b97c3cb6e882e52bbe3a
MarcAleVargas <marcosalevargas@icloud.com>
2023-09-27 06:41:49 -0400

    Update oracle_odbc_database_connections.py

--- oracle_odbc_database_connections.py
+++ oracle_odbc_database_connections.py
@@ -6,44 +6,37 @@ using this code you will be able to connect to different database
 once is connected you need to read you SQL and put together the connection like: pd.read_sql(sql_statement,connection)
 """
 
-class database_credential():
-    def __init__(self, user, password, server):
+class database():
+    def __init__(self, user, password, server, database):
         #Before to run this make sure you have the access and drivers to connect to the database
-
         #// database Credentials
         self.database_user = user
         self.database_password = password
         self.database_server = server
-
-    def oracle(self):
+        self.database = database
+    
+    def connection(self):
         try:
-            self.connection = cx_Oracle.connect(self.database_user,
-                                self.database_password,
-                                self.database_server)
-            self.cursor = self.connection.cursor()
+            if self.database == 'oracle_(Library_Name)':
+                self.connection = cx_Oracle.connect(self.database_user,
+                                    self.database_password,
+                                    self.database_server)
+                self.cursor = self.connection.cursor()
+
+            elif self.database == 'odbc_(Library_Name)':
+                    self.connection = pyodbc.connect(
+                        f"""DSN={self.self.database_server};
+                        UID={self.self.database_user};
+                        PWD={self.database_password}"""
+                        )
+                    self.cursor = self.connection.cursor()
             print("Connection Successfully")
             return(self.connection)
         except:
             print("Connection Lost")
     
-    def odbc(self):
-        try:
-            self.connection = pyodbc.connect(
-                f"""DSN={self.self.database_server};
-                UID={self.self.database_user};
-                PWD={self.database_password}"""
-                )
-            self.cursor = self.connection.cursor()
-            print("Connection Successfully")
-            return(self.connection)
-        except:
-            print("Connection Lost")
 
-
-# database_credential('user', 'password', 'server').oracle()
-# database_credential('user', 'password', 'server').odbc()
-
-#Best practices rehuse your code by create classes
+#database().connection()
 
 """
 You can set up your credentials in the enviroment so you won't be rewriting everything your credentials and separete each database with a different class in order to pass the right credentials.
