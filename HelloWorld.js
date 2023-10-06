d0f0918a6711e93bbda30129e7827b76a47dcb49
MarcAleVargas <marcosalevargas@icloud.com>
2023-10-06 19:35:53 -0400

    dev 106

--- HelloWorld.js
+++ HelloWorld.js
@@ -1,1 +1,7 @@
-console.log("\nhello, world\n")
+console.log("\nhello, world\n");
+
+function soluction(data){
+    values = Math.max(...data.slice(1).map((x,i)=>[x*data[i]]));
+    return values
+}
+console.log(soluction([3, 6, -2, -5, 7, 3]))
--- HelloWorld.py
+++ HelloWorld.py
@@ -1,1 +1,6 @@
 print("\nhello, world\n")
+
+def soluction(data):
+    values = max([x*data[i] for i, x in enumerate(data[1:])])
+    return values
+print(soluction([3, 6, -2, -5, 7, 3]))
